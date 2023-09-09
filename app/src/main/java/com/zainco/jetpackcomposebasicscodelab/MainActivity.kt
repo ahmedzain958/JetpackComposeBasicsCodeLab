4a155eddf172149b89bfd3cac05653fdd8531248
Zain <ahmed.zaineldin@vodafone.com>
2023-09-09 16:10:07 +0300

    extra padding with expanded

--- app/src/main/java/com/zainco/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/zainco/jetpackcomposebasicscodelab/MainActivity.kt
@@ -3,19 +3,18 @@ package com.zainco.jetpackcomposebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.zainco.jetpackcomposebasicscodelab.ui.theme.JetpackComposeBasicsCodeLabTheme
@@ -25,6 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeBasicsCodeLabTheme {
+                MyApp()
             }
         }
     }
@@ -33,9 +33,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = listOf("World", "Compose"),
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
+    Column(
+        modifier = modifier
+            .padding(vertical = 4.dp)
+            .fillMaxSize()
+    ) {
         for (name in names) {
             Greeting(name = name)
         }
@@ -44,14 +48,27 @@ fun MyApp(
 
 @Composable
 private fun Greeting(name: String) {
+    val isExpanded = remember {
+        mutableStateOf(false)
+    }
+    val extraPadding = if (isExpanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Text(text = "Hello, ")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { isExpanded.value = !isExpanded.value }) {
+                Text(if (isExpanded.value) "Show Less" else "Show more")
+            }
         }
+        /*    Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }*/
     }
 }
 
