ac074eb4b63f5823c9d1cb9ccfa1f9687111b29d
Zain <ahmed.zaineldin@vodafone.com>
2023-11-18 22:25:37 +0200

    SwipeablePages

--- app/src/main/java/com/zainco/jetpackcomposebasicscodelab/ui/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/zainco/jetpackcomposebasicscodelab/ui/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,15 +1,25 @@
 package com.zainco.jetpackcomposebasicscodelab.ui.newsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScopeInstance.weight
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.unit.dp
 import com.zainco.jetpackcomposebasicscodelab.ui.newsapp.pages
+import com.zainco.jetpackcomposebasicscodelab.ui.newsapp.presentation.onboarding.componenets.OnBoardingPage
+import com.zainco.jetpackcomposebasicscodelab.ui.newsapp.presentation.onboarding.componenets.PagerIndicator
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -28,8 +38,12 @@ fun OnBoardingScreen() {
                 }
             }
         }
-        HorizontalPager(state = pagerState) {
-
+        HorizontalPager(state = pagerState) { index ->
+            OnBoardingPage(page = pages[index])
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Row (modifier = Modifier.fillMaxWidth()){
+            PagerIndicator(modifier = Modifier.width(52.dp), pagesSize = pages.size, selectedPage = pagerState.currentPage)
         }
     }
 }
--- app/src/main/java/com/zainco/jetpackcomposebasicscodelab/ui/theme/Theme.kt
+++ app/src/main/java/com/zainco/jetpackcomposebasicscodelab/ui/theme/Theme.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
+/*private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
@@ -26,23 +26,27 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
+)*/
+//for news app
+private val DarkColorScheme = darkColorScheme(
+    primary = Blue,
+    background = Black,
+    error = DarkRed,
+    surface = LightBlack
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val LightColorScheme = lightColorScheme(
+    primary = Blue,
+    background = Color.White,
+    error = LightRed,
+    surface = Color.White
 )
 
 @Composable
 fun JetpackComposeBasicsCodeLabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
