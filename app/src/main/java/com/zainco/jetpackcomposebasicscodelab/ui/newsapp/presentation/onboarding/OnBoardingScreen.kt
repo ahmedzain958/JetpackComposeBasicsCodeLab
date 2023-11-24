0854e91b9a7567402e4625298c4a4c9db2d7e2f0
Zain <ahmed.zaineldin@vodafone.com>
2023-11-24 20:49:52 +0200

    fix row arrangement and alignment

--- app/src/main/java/com/zainco/jetpackcomposebasicscodelab/ui/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/zainco/jetpackcomposebasicscodelab/ui/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,13 +1,14 @@
 package com.zainco.jetpackcomposebasicscodelab.ui.newsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScopeInstance.weight
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -15,9 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.unit.dp
+import com.zainco.jetpackcomposebasicscodelab.ui.newsapp.Dimens.MediumPadding2
 import com.zainco.jetpackcomposebasicscodelab.ui.newsapp.common.NewsButton
 import com.zainco.jetpackcomposebasicscodelab.ui.newsapp.common.NewsTextButton
 import com.zainco.jetpackcomposebasicscodelab.ui.newsapp.pages
@@ -46,13 +49,18 @@ fun OnBoardingScreen() {
             OnBoardingPage(page = pages[index])
         }
         Spacer(modifier = Modifier.weight(1f))
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = MediumPadding2)
+            .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             PagerIndicator(
                 modifier = Modifier.width(52.dp),
                 pagesSize = pages.size,
                 selectedPage = pagerState.currentPage
             )
-            Row {
+            Row (verticalAlignment = Alignment.CenterVertically){
                 val scope = rememberCoroutineScope()
                 if (buttonState.value[0].isNotEmpty()) {
                     NewsTextButton(text = buttonState.value[0], onClick = {
