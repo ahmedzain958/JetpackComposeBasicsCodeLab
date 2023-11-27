5fdef69440b8afe7fe9efd20e8d2a5f5c482feff
Zain <ahmed.zaineldin@vodafone.com>
2023-11-27 21:20:29 +0200

    jump game 2

--- app/src/main/java/com/zainco/jetpackcomposebasicscodelab/problem_solving/Solution.kt
+++ app/src/main/java/com/zainco/jetpackcomposebasicscodelab/problem_solving/Solution.kt
@@ -1,9 +1,27 @@
 package com.zainco.jetpackcomposebasicscodelab.problem_solving
 
 fun main() {
-    val canJumb = canJump(intArrayOf(2,3,1,1,4))
+    println(" ${canJump(intArrayOf(2,3,1,1,4))}")
+    //Jump GAme 2
+    println("Jump Game II ${jump(intArrayOf(2,3,1,1,4))}")
 }
+fun jump(nums: IntArray): Int {
+    val n = nums.size
+    var maxReach = 0
+    var steps = 0
+    var lastJump = 0
 
+    for (i in 0 until n - 1) {
+        maxReach = maxOf(maxReach, i + nums[i])
+
+        if (i == lastJump) {
+            lastJump = maxReach
+            steps++
+        }
+    }
+
+    return steps
+}
 fun canJump(nums: IntArray): Boolean {
     var i = 0
     var maxReach = 0
@@ -16,3 +34,4 @@ fun canJump(nums: IntArray): Boolean {
         return true
     return false
 }
+
